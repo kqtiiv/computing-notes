1efeea2198a2c65f318647500f68d0c4b1b2861f
Katie Wan <kw1425@ic.ac.uk>
2025-10-10 17:31:49 +0100

    Add haskell week 2 notes

---
+++ Year 1/COMP40009 Computing Practical 1/Lectures/week2.hs
@@ -0,0 +1,33 @@
+module Week2 (Day(..)) where -- exporting modles
+-- the (..) specifies that i want to use EVERY data dype
+-- i can also do (Mon, Tue) if i only want to use monday and tuesday
+
+-- modules do not export its imports, so you would have to export it yourself if you want to
+> module Week2 (module Day) where
+> import Day
+
+data Day = Mon | Tue | Wed | Thur | Fri | Sat | Sun
+
+import Data.Char (chr, ord) -- importing functions from packages
+import Prelude hiding (zip, unzip)-- always automatically imported
+-- only reason why you would want to import it is if you want to remove functions from it
+
+import Data.Char qualified as Katie
+-- this does not introduce every single function into our namespace
+-- we would have to define it as below
+-- 
+ord = Katie.ord
+
+-- try not to use the !! operator, as it is an expensive function
+
+zip :: [a] -> [b] -> [(a, b)]
+zip (x:xs) (y:ys) = (x, y) : zip xs ys
+zip _      _      = []
+
+zip (1:2:3:[]) (True:False:[]) = 
+    (1, True) : zip (2:3:[]) (False:[]) =
+    (1, True) : (2, False) : zip (3:[]) [] =
+    (1, True) : (2, False) : [] =
+    (1, True) : (2, False)
+
+> 7 :: Num a -> a -- 7 has type a, but a is constrained by Num
